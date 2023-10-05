cf69288438a52102194167e572d93fc483fad533
ParasArya289 <82272875+ParasArya289@users.noreply.github.com>
2023-10-05 09:09:12 +0000

    feat: setting up express app

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 require("./mongodb")
+const express = require("express");
+const helmet = require("helmet");
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+
+app.get("/",(req,res)=>{
+  res.send("Fitness tracker backend")
+})
+
+//Global error handler
+app.use((err,req,res,next)=>{
+  res.status(500).json({error:"Something went wrong!"})
+})
+
+//Global route handler
+app.use((req,res)=>{
+  res.status(404).json({error:"Requested API endpoint does not exist!"})
+})
+
+app.listen(3000,()=>{
+  console.log("Server started")
+})
